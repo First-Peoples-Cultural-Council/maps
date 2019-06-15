32d93329b8e6e8abae93b575b4f183946dafaa9d
Clark Van Oyen <clark@countable.ca>
2019-06-14 21:33:49 -0700

    Make zooming to location actually work

--- convert.py
+++ convert.py
@@ -7,19 +7,20 @@ out = {
   "type": "FeatureCollection",
   "features": []
 }
+kind=sys.argv[1].split('.')[0]
 
 for c in C:
     f = {"type":"Feature"}
     f['geometry'] = json.loads(c['geoJSON'])
-    f['properties'] = {}
-    if 'title' in c:
-        f['properties']['title'] = c['title']
+    f['properties'] = {"kind": kind}
     if 'color' in c:
         f['properties']['color'] = c['color']
-    if 'popup' in c:
-        f['properties']['popup'] = HTMLParser().unescape(c['popup'].split('>')[1].split('<')[0])
-    if 'description' in c:
-        f['properties']['description'] = HTMLParser().unescape(c['description'].split('>')[1].split('<')[0])
+    if 'title' in c:
+        f['properties']['title'] = c['title']
+    elif 'popup' in c:
+        f['properties']['title'] = HTMLParser().unescape(c['popup'].split('>')[1].split('<')[0])
+    elif 'description' in c:
+        f['properties']['title'] = HTMLParser().unescape(c['description'].split('>')[1].split('<')[0])
     out['features'].append(f)
 
 print(json.dumps(out))
