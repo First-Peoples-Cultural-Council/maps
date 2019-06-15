762fab865f8c8d0c265ce7c095b6e537227103d0
Clark Van Oyen <clark@countable.ca>
2019-06-14 19:44:33 -0700

    clean data

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
-import json
+from html.parser import HTMLParser
+import json, sys
 
-C=json.loads(open('langs.raw').read())
+C=json.loads(open(sys.argv[1]).read())
 
 out = {
   "type": "FeatureCollection",
@@ -10,7 +11,15 @@ out = {
 for c in C:
     f = {"type":"Feature"}
     f['geometry'] = json.loads(c['geoJSON'])
-    f['properties'] = {'title':c['title'],'color':c['color']}
+    f['properties'] = {}
+    if 'title' in c:
+        f['properties']['title'] = c['title']
+    if 'color' in c:
+        f['properties']['color'] = c['color']
+    if 'popup' in c:
+        f['properties']['popup'] = HTMLParser().unescape(c['popup'].split('>')[1].split('<')[0])
+    if 'description' in c:
+        f['properties']['description'] = HTMLParser().unescape(c['description'].split('>')[1].split('<')[0])
     out['features'].append(f)
 
 print(json.dumps(out))
