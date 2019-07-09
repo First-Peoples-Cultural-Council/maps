33d8ebf44b600d82118da4bf112a5482a61024d6
Aaron <aaron@countable.ca>
2019-07-08 23:22:11 -0700

    more stuff

---
+++ frontend/components/languages/LanguageSeeAll.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="lang-see-all">
+    <span>{{ content }}</span>
+    <span><img src="@/assets/images/go_icon.svg" alt="Go"/></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    content: {
+      default: '',
+      type: String
+    }
+  }
+}
+</script>
+
+<style>
+.lang-see-all {
+  line-height: 0;
+  display: flex;
+  justify-content: space-between;
+  background-color: var(--color-beige);
+  align-items: center;
+  font-size: 0.9em;
+  padding: 0.5em 0.5em;
+  font-weight: bold;
+  color: var(--color-gray);
+  border-radius: 0.25em;
+  cursor: pointer;
+}
+</style>
--- frontend/pages/index/languages/_lang/index.vue
+++ frontend/pages/index/languages/_lang/index.vue
@@ -1,45 +1,74 @@
 <template>
   <div class="language">
-    <div
-      class="color-row"
-      :style="'background-color: ' + getColor(languages)"
-      @click="handleColor"
-    ></div>
+    <div class="color-row" :style="'background-color: ' + languageColor"></div>
     <LanguageDetailCard
-      :color="getColor(languages)"
+      :color="languageColor"
       :name="this.$route.params.lang"
     ></LanguageDetailCard>
-    <h5 class="other-lang-names-title text-uppercase mt-4 ml-2">
-      Other Language Names
-    </h5>
+    <section class="ml-2 mr-2">
+      <h5 class="other-lang-names-title text-uppercase mt-4">
+        Other Language Names
+      </h5>
+      <LanguageDetailBadge
+        v-for="(name, index) in otherNames"
+        :key="index"
+        :content="name"
+        class="mr-2"
+      ></LanguageDetailBadge>
+      <LanguageSummary
+        population="121"
+        speakers="121"
+        somewhat="121"
+        learners="87"
+        class="mt-4"
+      ></LanguageSummary>
+      <LanguageSeeAll
+        content="See all details"
+        class="mt-3"
+        @click.native="handleMoreDetails"
+      ></LanguageSeeAll>
+    </section>
   </div>
 </template>
 
 <script>
 import LanguageDetailCard from '@/components/languages/LanguageDetailCard.vue'
 import { mapState } from 'vuex'
+import LanguageDetailBadge from '@/components/languages/LanguageDetailBadge.vue'
+import LanguageSummary from '@/components/languages/LanguageSummary.vue'
+import LanguageSeeAll from '@/components/languages/LanguageSeeAll.vue'
 export default {
   components: {
-    LanguageDetailCard
+    LanguageDetailCard,
+    LanguageDetailBadge,
+    LanguageSummary,
+    LanguageSeeAll
   },
   computed: {
     ...mapState({
       communities: state => state.communities.communities,
-      languages: state => state.languages.languages
+      languages: state => state.languages.languages,
+      language(state) {
+        return state.languages.languages.find(
+          lang => lang.name === this.$route.params.lang
+        )
+      },
+      otherNames() {
+        return this.language.other_names.split('/')
+      },
+      languageColor() {
+        return this.language.color
+      }
     })
   },
   mounted() {
     console.log('Store', this.$store.state.languages)
   },
   methods: {
-    handleColor() {
-      console.log('Color Click')
-      console.log(this.communities)
-      console.log('Language', this.languages)
-      console.log('Language Color', this.languageColor)
-    },
-    getColor(languages) {
-      return languages.find(lang => lang.name === this.$route.params.lang).color
+    handleMoreDetails() {
+      this.$router.push({
+        path: `${encodeURIComponent(this.$route.params.lang)}/details`
+      })
     }
   }
 }
