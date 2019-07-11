909ccda430ef46dd2676a76fe3716a17319c61f1
Clark Van Oyen <clark@countable.ca>
2019-07-10 17:47:32 -0700

    inline lang family

--- web/language/serializers.py
+++ web/language/serializers.py
@@ -1,9 +1,24 @@
-from .models import Language, PlaceName, Community, Champion
+from .models import Language, PlaceName, Community, Champion, LanguageSubFamily, LanguageFamily
 from rest_framework import serializers
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
 
 
+class LanguageFamilySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = LanguageFamily
+        fields = ('name',)
+
+
+class LanguageSubFamilySerializer(serializers.ModelSerializer):
+    family = LanguageFamilySerializer(read_only=True)
+    class Meta:
+        model = LanguageSubFamily
+        fields = ('name','family')
+
+
 class LanguageSerializer(serializers.ModelSerializer):
+    sub_family = LanguageSubFamilySerializer(read_only=True)
     class Meta:
         model = Language
         fields = ('name', 'other_names',
