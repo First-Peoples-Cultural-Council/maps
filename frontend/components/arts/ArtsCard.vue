7e2293c38bc5a94cec7efdc4d5ac45c38c690702
Aaron <aaron@countable.ca>
2019-07-24 16:30:05 -0700

    more uniform

--- frontend/components/arts/ArtsCard.vue
+++ frontend/components/arts/ArtsCard.vue
@@ -43,7 +43,7 @@
       </template>
       <template v-slot:footer>
         <div class="fpcc-card-more">
-          <img v-if="!hover" src="@/assets/images/go_icon.svg" alt="Go" />
+          <img v-if="!hover" src="@/assets/images/go_icon_hover.svg" alt="Go" />
           <img v-else src="@/assets/images/go_icon_hover.svg" alt="Go" />
         </div>
       </template>
@@ -111,7 +111,7 @@ export default {
   height: 100%;
 }
 .fpcc-card-more {
-  background-color: var(--color-beige);
+  background-color: #c46156;
   display: flex;
   align-items: center;
   height: 35px;
