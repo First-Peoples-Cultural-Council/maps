1d1f07b78cf8d3400d5eb4f9e6306774b7ac9058
Aaron <aaron@countable.ca>
2019-07-29 15:08:03 -0700

    remove prounounce

--- frontend/components/languages/LanguageDetailCard.vue
+++ frontend/components/languages/LanguageDetailCard.vue
@@ -21,9 +21,6 @@
               {{ name }}
             </h5>
           </div>
-          <div class="d-inline-block" @click.prevent.stop="handlePronounce">
-            <CardBadge content="Pronounce"></CardBadge>
-          </div>
         </div>
       </template>
       <template v-slot:footer>
@@ -54,11 +51,9 @@
 
 <script>
 import Card from '@/components/Card.vue'
-import CardBadge from '@/components/CardBadge.vue'
 export default {
   components: {
-    Card,
-    CardBadge
+    Card
   },
   props: {
     name: {
