cd9e064b678c5679939705a254bd86597313ae57
Denis Calixto <deniscalixto@gmail.com>
2019-08-13 16:03:57 -0700

    Changing the basename of one of the routes

--- web/language/urls.py
+++ web/language/urls.py
@@ -9,7 +9,7 @@ from .views import LanguageViewSet, CommunityViewSet
 
 router = routers.DefaultRouter()
 router.register(r"language", LanguageViewSet, basename="language")
-router.register(r"community", CommunityViewSet, basename="language")
+router.register(r"community", CommunityViewSet, basename="community")
 
 urlpatterns = [
     url("language-geo/$", LanguageGeoList.as_view(), name="language-geo"),
