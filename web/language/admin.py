99b0c5253d23f61bb1cb83784d7b9763c236753e
Denis Calixto <deniscalixto@gmail.com>
2019-08-18 17:23:47 -0700

    Registering CommunityMember for Django Admin

--- web/language/admin.py
+++ web/language/admin.py
@@ -4,11 +4,14 @@ from .models import (
     Language,
     LanguageFamily,
     Community,
+    CommunityMember,
     Dialect,
     PlaceName,
+    PlaceNameCategory,
     Champion,
     LNA,
     LNAData,
+    Media,
 )
 
 
@@ -22,8 +25,10 @@ class LNADataAdmin(admin.ModelAdmin):
 
 admin.site.register(Champion)
 admin.site.register(PlaceName)
+admin.site.register(PlaceNameCategory)
 admin.site.register(Language, LanguageAdmin)
 admin.site.register(LanguageFamily)
 admin.site.register(Community)
+admin.site.register(CommunityMember)
 admin.site.register(LNA)
 admin.site.register(LNAData, LNADataAdmin)
