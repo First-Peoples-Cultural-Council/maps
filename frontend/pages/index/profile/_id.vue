395be5e5accd682f695ce3060295c57987ed906b
Aaron <aaron@countable.ca>
2019-08-31 20:03:04 -0700

    encode nuxt link

--- frontend/pages/index/profile/_id.vue
+++ frontend/pages/index/profile/_id.vue
@@ -2,8 +2,8 @@
   <DetailSideBar>
     <UserDetailCard :name="getUserName()" type="none"></UserDetailCard>
     <ul>
-      <li :key="placename.id" v-for="placename in user.placename_set">
-        <nuxt-link :to="'/place-names/' + placename.name">{{
+      <li v-for="placename in user.placename_set" :key="placename.id">
+        <nuxt-link :to="'/place-names/' + encodeFPCC(placename.name)">{{
           placename.name
         }}</nuxt-link>
       </li>
@@ -14,17 +14,16 @@
 <script>
 import DetailSideBar from '@/components/DetailSideBar.vue'
 import UserDetailCard from '@/components/user/UserDetailCard.vue'
-import { getApiUrl } from '@/plugins/utils.js'
+import { getApiUrl, encodeFPCC } from '@/plugins/utils.js'
 
 export default {
-  async asyncData({ params, $axios, store }) {
-    const user = await $axios.$get(getApiUrl(`user/${params.id}/`))
-    return { user }
-  },
-
   components: {
     DetailSideBar,
     UserDetailCard
+  },
+  async asyncData({ params, $axios, store }) {
+    const user = await $axios.$get(getApiUrl(`user/${params.id}/`))
+    return { user }
   },
 
   mounted() {
@@ -36,7 +35,8 @@ export default {
       return (
         this.user && (this.user.first_name || this.user.username.split('__')[0])
       )
-    }
+    },
+    encodeFPCC
   }
 }
 </script>
