acfbc2af80ae004f7d22e1eee7b5d6e1623b9547
Denis Calixto <deniscalixto@gmail.com>
2019-09-04 18:39:05 -0700

    User's bio can be NULL

--- web/users/models.py
+++ web/users/models.py
@@ -47,7 +47,7 @@ class User(AbstractUser):
     )
 
     languages = models.ManyToManyField("language.Language")
-    bio = models.TextField(default="")
+    bio = models.TextField(null=True, default="")
 
 
 # email = models.EmailField(
