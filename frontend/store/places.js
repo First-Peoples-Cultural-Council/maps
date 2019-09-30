dbb9da060fc0a0af5d8b94c5cc8a0858101d8121
Aaron <aaron@countable.ca>
2019-09-30 14:00:13 -0700

    favourites working

--- frontend/store/places.js
+++ frontend/store/places.js
@@ -1,11 +1,12 @@
-import { getApiUrl } from '@/plugins/utils.js'
+import { getApiUrl, getCookie } from '@/plugins/utils.js'
 
 export const state = () => ({
   place: null,
   placeCommunity: null,
   places: [],
   placesSet: [],
-  medias: []
+  medias: [],
+  favourites: []
 })
 
 export const mutations = {
@@ -27,6 +28,10 @@ export const mutations = {
 
   setMedias(state, medias) {
     state.medias = medias
+  },
+
+  setFavourites(state, favourites) {
+    state.favourites = favourites
   }
 }
 
@@ -62,6 +67,43 @@ export const actions = {
   async getPlaceCommunity({ commit }, data) {
     const result = await this.$axios.$get(getApiUrl(`community/${data.id}/`))
     commit('setPlaceCommunity', result)
+    return result
+  },
+
+  async getFavourites({ commit }, data) {
+    const result = await this.$axios.$get(
+      getApiUrl(`favourite?timestamp=${new Date().getTime()}/`)
+    )
+    commit('setFavourites', result)
+    return result
+  },
+
+  async setFavourite({ commit, dispatch }, data) {
+    const headers = {
+      headers: {
+        'X-CSRFToken': getCookie('csrftoken')
+      }
+    }
+    const result = await this.$axios.post(
+      getApiUrl(`favourite/`),
+      data,
+      headers
+    )
+    await dispatch('places/getFavourites', {}, { root: true })
+    return result
+  },
+
+  async removeFavourite({ commit, dispatch }, data) {
+    const headers = {
+      headers: {
+        'X-CSRFToken': getCookie('csrftoken')
+      }
+    }
+    const result = await this.$axios.delete(
+      getApiUrl(`favourite/${data.favourite.id}`),
+      headers
+    )
+    await dispatch('places/getFavourites', {}, { root: true })
     return result
   }
 }
