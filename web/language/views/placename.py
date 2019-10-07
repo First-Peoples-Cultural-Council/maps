8e23cad54b9f40acf9056015512df798b08f3548
Denis Calixto <deniscalixto@gmail.com>
2019-10-07 00:21:26 -0700

    Fixing placename-geo api list for non-Administrators

--- web/language/views/placename.py
+++ web/language/views/placename.py
@@ -208,7 +208,7 @@ class PlaceNameGeoList(generics.ListAPIView):
                 )
                 queryset = queryset_user.union(queryset_community).union(queryset_admin)
             else: #user is not Administrator of anything
-                queryset = queryset_user.union(queryset_community).union(queryset_admin)
+                queryset = queryset_user.union(queryset_community)
 
         else: #no user is logged in
             queryset = queryset.filter(
