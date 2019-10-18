b68b476ef2204e8f2131b622f77b01d3e830b765
Aaron <aaron@countable.ca>
2019-10-17 21:20:35 -0700

    tos

--- frontend/pages/index/tos.vue
+++ frontend/pages/index/tos.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>Terms of Service</div>
+  <div class="p-2">
+    <div v-html="$md.render(tos.content)"></div>
+  </div>
 </template>
 <script>
-export default {}
+import { getApiUrl } from '@/plugins/utils.js'
+
+export default {
+  async asyncData({ params, $axios, store }) {
+    const result = await $axios.$get(`${getApiUrl(`page/`)}`)
+    const tos = result.find(r => r.name === 'tos')
+    return {
+      tos
+    }
+  }
+}
 </script>
 <style></style>
