d62412234a3456f897517e33212e1a7c9f49e247
Aaron <aaron@countable.ca>
2019-10-22 21:43:11 -0700

    more obivous notificaitons

--- frontend/pages/index/content/_fn.vue
+++ frontend/pages/index/content/_fn.vue
@@ -29,6 +29,17 @@
       ></CommunityDetailCard>
       <hr class="sidebar-divider mt-0" />
       <Filters class="mb-3"></Filters>
+      <section>
+        <Notification
+          v-if="isLoggedIn"
+          :id="community.id"
+          :is-server="isServer"
+          type="community"
+          class="m-4"
+          :unsubscribe="!!subscribed"
+          :subscription="subscribed"
+        ></Notification>
+      </section>
       <section class="pl-3 pr-3">
         <div v-if="otherNames">
           <h5 class="other-lang-names-title text-uppercase mt-">
@@ -198,17 +209,6 @@
             <hr class="mb-2" />
           </div>
         </div>
-      </section>
-      <section>
-        <Notification
-          v-if="isLoggedIn"
-          :id="community.id"
-          :is-server="isServer"
-          type="community"
-          class="m-4"
-          :unsubscribe="!!subscribed"
-          :subscription="subscribed"
-        ></Notification>
       </section>
     </div>
   </div>
