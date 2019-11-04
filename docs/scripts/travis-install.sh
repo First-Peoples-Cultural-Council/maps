54275f0be90cc5fa8145e7784542eaad3ef52a7f
Clark Van Oyen <clark@countable.ca>
2019-11-04 12:12:57 -0800

    Tell travis what's up

--- docs/scripts/travis-install.sh
+++ docs/scripts/travis-install.sh
@@ -2,5 +2,6 @@
 sudo apt-get update
 sudo apt-get install -y libproj-dev gdal-bin libpq-dev libpython3-dev \
     default-libmysqlclient-dev build-essential
+echo "installed OS deps"
 cd web
 pip install -r requirements.txt
