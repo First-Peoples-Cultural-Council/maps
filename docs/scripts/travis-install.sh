543c593444d7a8d0bdd54886c8acb4b0e0f05c3e
Clark Van Oyen <clark@countable.ca>
2019-11-04 11:47:27 -0800

    update travis setup

--- docs/scripts/travis-install.sh
+++ docs/scripts/travis-install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-
+sudo apt-get update
+sudo apt-get install -y libproj-dev gdal-bin libpq-dev libpython3-dev \
+    default-libmysqlclient-dev build-essential
 cd web
 pip install -r requirements.txt
