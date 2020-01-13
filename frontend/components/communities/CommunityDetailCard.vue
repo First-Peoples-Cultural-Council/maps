ea80faa1cb4477286cbad11762356c2497fe3b85
Aaron Ahn <aaron@countable.ca>
2020-01-13 10:53:21 -0800

    remove inlinb elbock

--- frontend/components/communities/CommunityDetailCard.vue
+++ frontend/components/communities/CommunityDetailCard.vue
@@ -21,11 +21,7 @@
               {{ name }}
             </h5>
           </div>
-          <div
-            v-if="audioFile"
-            class="d-inline-block"
-            @click.prevent.stop="handlePronounce"
-          >
+          <div v-if="audioFile" @click.prevent.stop="handlePronounce">
             <CardBadge content="Pronounce"></CardBadge>
           </div>
           <div v-if="population" class="d-inline-block">
