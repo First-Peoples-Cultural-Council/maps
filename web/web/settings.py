3f200a369961ef777f1b9e0a59a3f485589b1211
Nolan Van Hell <nolan@fpcc.ca>
2020-01-14 15:18:34 -0800

    FW-738: Update maps-dev.fpcc.ca to maps.fpcc.ca

--- web/web/settings.py
+++ web/web/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "**5ghswp2+x=2(3)m&y+&012y6qiirl6_d3t6
 # env var, DEBUG=0 for False, DEBUG=1 for True.
 DEBUG = bool(int(os.environ.get("DEBUG", "1")))
 
-HOST = os.environ.get("HOST", 'https://maps-dev.fpcc.ca')
+HOST = os.environ.get("HOST", 'https://maps.fpcc.ca')
 
 ALLOWED_HOSTS = ["*"]
 
