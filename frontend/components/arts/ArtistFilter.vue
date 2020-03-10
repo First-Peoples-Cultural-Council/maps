dacf701d02b215146018e888cea7f105b55cd091
Mico Ang <mico@countable.ca>
2020-03-11 03:56:22 +0800

    [UPDATE] Arts module Artist Only/Show All Filter but no functionality

---
+++ frontend/components/arts/ArtistFilter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="filter-container">
+    <b-badge class="badge filter-selected">
+      <span class="badge-content">Artworks Only</span>
+    </b-badge>
+    <b-badge class="badge ">
+      <span class="badge-content ">Show All</span>
+    </b-badge>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.filter-container {
+  display: flex;
+  width: 100%;
+  background-color: #eee;
+  margin: 0.5em 0;
+  border-radius: 1em;
+}
+
+.filter-container > .badge {
+  flex: 1 1;
+  padding: 0.5em;
+  margin: 1px;
+  background-color: #eee;
+  color: #000;
+  font-weight: bold;
+  transition: 0.5s ease-in;
+}
+
+.badge.filter-selected {
+  background-color: #b57936;
+  color: #fff;
+}
+</style>
