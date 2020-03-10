229ad9d874feb7262445a1755ffb75d2187e57ff
Mico Ang <mico@countable.ca>
2020-03-11 03:53:03 +0800

    [UPDATE] Added Artist detail banner template

---
+++ frontend/components/arts/ArtistBanner.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="arts-artist-banner">
+    <div class="arts-artist-header">
+      <slot name="header"></slot>
+    </div>
+    <div class="arts-artist-body"><slot name="body"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    variant: {
+      default: 'normal',
+      type: String
+    }
+  }
+}
+</script>
+
+<style>
+.arts-artist-banner {
+  /* border: 1px solid red; */
+}
+.arts-artist-header {
+  position: relative;
+  border-bottom: 0.8px solid rgb(0, 0, 0, 0.25);
+}
+
+.arts-artist-body {
+  width: 100%;
+  height: 100px;
+  position: relative;
+}
+
+.arts-artist-content {
+  width: 100%;
+  position: absolute;
+  top: -3rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-items: center;
+}
+</style>
