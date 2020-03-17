3584d1b1cfc514ef6da104c1fe6156f8a60cf99b
Mico Ang <mico@countable.ca>
2020-03-18 01:03:28 +0800

    [UPDATE] Finished Event Menu, and Event Pop Up feature

---
+++ frontend/components/Event.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="navbar-container cursor-pointer hide-mobile">
+    <nav id="event-icon-container">
+      <span>NEXT EVENTS</span>
+      <img src="@/assets/images/menu_icon.svg" alt="Menu" class="navbar-icon" />
+    </nav>
+    <b-popover
+      target="event-icon-container"
+      placement="bottom"
+      triggers="click"
+      :show.sync="showEvents"
+    >
+      <EventCard />
+      <EventCard />
+    </b-popover>
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      showEvents: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#event-icon-container {
+  position: fixed;
+  top: 10px;
+  right: 100px;
+  background-color: white;
+  padding: 1em;
+  z-index: 50;
+  border: 1px solid #ddd5cc;
+  border-radius: 2em;
+  color: #7b7b7b;
+  font-size: 13px;
+  font-weight: bold;
+  display: flex;
+  justify-content: space-evenly;
+
+  & > * {
+    margin: 0 0.25em;
+  }
+}
+
+.navbar-icon {
+  display: inline-block;
+  width: 15px;
+  height: 15px;
+  line-height: 0;
+  padding: 0;
+  margin: 0;
+}
+.popover {
+  width: 350px;
+  max-width: 350px;
+  max-height: 800px;
+  overflow-y: auto;
+}
+
+.popover-body {
+}
+</style>
---
+++ frontend/components/EventCard.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="event-card-container">
+    <span class="event-date">Saturday 21TH-2020 - 13:00</span>
+    <h4 class="event-title">'Q'emcin 2 Rivers REmix 2019</h4>
+    <div class="events-tags-container">
+      <span>Artist</span>
+      <span>Visual</span>
+      <span>Painter</span>
+      <span>Carver</span>
+      <span>Carver</span>
+    </div>
+    <img src="@/assets/images/sample-dp.jpg" />
+    <p class="event-description">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, unde qui
+      quos iste praesentium laborum? Aliquam at fuga veniam quis id, neque quod
+      alias obcaecati porro suscipit ullam ea temporibus!
+    </p>
+    <button>CHECK EVENT</button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.event-card-container {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 1em 0;
+
+  .event-date {
+    font: Medium 13px/20px;
+    letter-spacing: 0.65px;
+    color: #707070;
+    text-transform: uppercase;
+  }
+
+  .event-title {
+    font: Bold 20px/20px 'Faustina';
+    letter-spacing: 1px;
+    color: #454545;
+  }
+
+  .event-description {
+    letter-spacing: 0.65px;
+    color: #707070;
+    padding: 5px;
+  }
+  button {
+    align-self: center;
+    background: #b47a2b 0% 0% no-repeat padding-box;
+    border: 1px solid #eeeae5;
+    border-radius: 2em;
+    color: #fff;
+    padding: 1em;
+    font-weight: bold;
+  }
+  img {
+    width: 100%;
+    height: 250px;
+    object-fit: contain;
+  }
+}
+
+.events-tags-container {
+  display: flex;
+  flex-wrap: wrap;
+  width: inherit;
+  margin: 0.5em 0;
+}
+
+.events-tags-container span {
+  flex: 0 1;
+  background: #ddd4c6 0% 0% no-repeat padding-box;
+  border-radius: 2rem;
+  color: #707070;
+  padding: 2px 6px;
+  font-weight: bold;
+  font-size: 0.8;
+  margin: 0.25em 0.25em 0.25em 0;
+  text-align: center;
+}
+</style>
