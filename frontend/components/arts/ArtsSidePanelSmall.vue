731889aec5388a1a118b58f7c88c747794c60645
Mico Ang <mico@countable.ca>
2020-03-27 04:07:23 +0800

    [UPDATE] Finished initial layout for SidePanel for List of Artist, and List of Medias

--- frontend/components/arts/ArtsSidePanelSmall.vue
+++ frontend/components/arts/ArtsSidePanelSmall.vue
@@ -1,46 +1,69 @@
 <template>
   <div class="arts-right-panel">
     <div class="panel-header">
-      <img class="artist-img-small" src="@/assets/images/arts/img-1.jpg" />
-      <div class="panel-details">
-        <span>Patrick Kelly</span>
-        <span>Hope (Status Krew)</span>
-        <div>Check Profile</div>
-      </div>
       <div class="panel-close-btn">
         <img src="@/assets/images/go_icon_hover.svg" alt="Go" />
         Close
       </div>
     </div>
-    <b-row class="m-1">
-      <b-col lg="12" xl="12" md="6" sm="6">
-        <ArtistCard class="mt-3 hover-left-move" :layout="'landscape'">
-        </ArtistCard>
-      </b-col>
-      <b-col lg="12" xl="12" md="6" sm="6">
-        <ArtistCard class="mt-3 hover-left-move" :layout="'landscape'">
-        </ArtistCard>
-      </b-col>
-      <b-col lg="12" xl="12" md="6" sm="6">
-        <ArtistCard class="mt-3 hover-left-move" :layout="'landscape'">
-        </ArtistCard>
+    <div v-for="artist in listOfArtists" :key="artist.id" class="panel-artist">
+      <img class="artist-img-small" :src="artist.image" />
+      <div class="panel-details">
+        <span>{{ artist.name }}</span>
+        <div>Check Profile</div>
+      </div>
+    </div>
+    <b-row class="m-1 media-list-container">
+      <b-col
+        v-for="media in listOfMedias"
+        :key="media.id"
+        lg="12"
+        xl="12"
+        md="6"
+        sm="6"
+      >
+        <MediaCard
+          class="mt-3 hover-left-move"
+          :media="media"
+          :geometry="art.geometry"
+        >
+        </MediaCard>
       </b-col>
     </b-row>
   </div>
 </template>
 
 <script>
-import ArtistCard from '@/components/arts/ArtistCard.vue'
+import MediaCard from '@/components/MediaCard.vue'
 
 export default {
   components: {
-    ArtistCard
+    MediaCard
+  },
+  props: {
+    art: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
   },
   data() {
     return {
       imgIndex: 0,
       showGallery: false
     }
+  },
+  computed: {
+    listOfArtists() {
+      return this.art.properties.artists
+    },
+    listOfMedias() {
+      return this.art.properties.medias
+    }
+  },
+  mounted() {
+    console.log('ARTS SIDE PANEL', this.art)
   }
 }
 </script>
@@ -49,12 +72,12 @@ export default {
 .arts-right-panel {
   display: flex;
   background: #f9f9f9 0% 0% no-repeat padding-box;
+  box-shadow: 0px 3px 6px #00000029;
+  border: 1px solid #d7d7de;
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
   height: 100%;
-  box-shadow: 0px 3px 6px #00000029;
-  border: 1px solid #d7d7de;
 }
 
 .panel-header {
@@ -63,7 +86,6 @@ export default {
   justify-content: flex-start;
   padding: 1em;
   position: relative;
-  border: 1px solid red;
 }
 
 .panel-details {
@@ -82,18 +104,26 @@ export default {
   text-align: center;
 }
 
+.panel-artist {
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+  padding: 1em;
+}
+
 .panel-close-btn {
   position: absolute;
   right: 0;
-  width: 30%;
-  background-color: #b57936;
+  width: 20%;
+  background-color: #953920;
   display: flex;
   align-items: center;
   height: 35px;
-  justify-content: center;
+  justify-content: space-around;
   border-top-left-radius: 1em;
   border-bottom-left-radius: 1em;
   color: #fff;
+  z-index: 50000;
 }
 
 .artist-img-small {
@@ -106,5 +136,9 @@ export default {
   display: flex;
   flex-direction: column;
   padding: 0.5em;
+}
+
+.media-list-container {
+  width: 100%;
 }
 </style>
