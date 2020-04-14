8e56d80fea77f286ed5009f8961a984d2ad7d5b0
Mico Ang <mico@countable.ca>
2020-04-15 04:04:17 +0800

    [UPDATE] Initial commit for BadgeFilter for taxonomy filter

---
+++ frontend/components/BadgeFilter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="badge-filter-container">
+    <slot name="badge"></slot>
+    <div class="badge-filters">
+      <!-- <p>choose sub-category</p> -->
+      <b-dropdown
+        size="sm"
+        variant="link"
+        toggle-class="text-decoration-none"
+        no-caret
+        dropleft
+      >
+        <template v-slot:button-content>
+          choose sub-category
+        </template>
+        <b-dropdown-item href="#">Administrator</b-dropdown-item>
+        <b-dropdown-item href="#">Artist</b-dropdown-item>
+      </b-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
