f5399f2a6ba97546fc08aaad63dc33cf87bdfb44
Mico Ang <mico@countable.ca>
2020-04-14 22:42:21 +0800

    [UPDATE] Edit on lazyloading

--- frontend/plugins/lazyloading.js
+++ frontend/plugins/lazyloading.js
@@ -1,11 +1,9 @@
 import VueLazyload from 'vue-lazyload'
 import Vue from 'vue'
-import error from '~/assets/images/artwork_icon.svg'
 import loading from '~/assets/images/loading.gif'
 
 Vue.use(VueLazyload, {
   preLoad: 1.3,
-  error,
   loading,
   attempt: 1
 })
