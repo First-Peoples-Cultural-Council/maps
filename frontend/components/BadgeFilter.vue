97fd0c90676d1292a49a1890d513c00b7531496d
Mico Ang <mico@countable.ca>
2020-04-17 01:03:25 +0800

    [UPDATE] Attempted nested sub-menu, tried different things, but failed

--- frontend/components/BadgeFilter.vue
+++ frontend/components/BadgeFilter.vue
@@ -2,20 +2,7 @@
   <div class="badge-filter-container">
     <slot name="badge"></slot>
     <div class="badge-filters">
-      <!-- <p>choose sub-category</p> -->
-      <b-dropdown
-        size="sm"
-        variant="link"
-        toggle-class="text-decoration-none"
-        no-caret
-        dropleft
-      >
-        <template v-slot:button-content>
-          choose sub-category
-        </template>
-        <b-dropdown-item href="#">Administrator</b-dropdown-item>
-        <b-dropdown-item href="#">Artist</b-dropdown-item>
-      </b-dropdown>
+      <p>choose sub-category</p>
     </div>
   </div>
 </template>
@@ -24,4 +11,26 @@
 export default {}
 </script>
 
-<style></style>
+<style lang="scss">
+.badge-filter-container {
+  display: flex;
+  position: relative;
+  align-items: center;
+  width: fit-content;
+  border: 2px solid #5a8467;
+  border-radius: 1em;
+  background-color: #ededed;
+  margin: 0.5em 0;
+  height: fit-content;
+}
+
+.badge-filters {
+  font-size: 12px;
+  margin: 0 0.5em;
+}
+
+.badge-filters > p {
+  margin: 0;
+  font-weight: 500;
+}
+</style>
