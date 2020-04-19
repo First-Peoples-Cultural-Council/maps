99b415b25174ea3202f514e1b23eeec13d4d220c
Justin Carretas <justin@countable.ca>
2020-04-19 23:49:40 +0800

    Update - Re-enabled list for PlaceNameViewset

--- web/language/views/placename.py
+++ web/language/views/placename.py
@@ -65,10 +65,7 @@ class PlaceNameFilterSet(FilterSet):
         fields = ('kinds', 'taxonomies')
 
 
-class PlaceNameViewSet(mixins.CreateModelMixin,
-                       mixins.RetrieveModelMixin,
-                       mixins.UpdateModelMixin,
-                       GenericViewSet):
+class PlaceNameViewSet(BaseModelViewSet):
     serializer_class = PlaceNameSerializer
     detail_serializer_class = PlaceNameDetailSerializer
     queryset = PlaceName.objects.all().order_by("name")
@@ -179,6 +176,77 @@ class PlaceNameViewSet(mixins.CreateModelMixin,
 
                     return Response(serializer.data)
         return Response([])
+
+    @method_decorator(never_cache)
+    def list(self, request):
+        queryset = self.get_queryset()
+        queryset = self.filter_queryset(queryset)
+
+        user_logged_in = False
+        if request and hasattr(request, "user"):
+            if request.user.is_authenticated:
+                user_logged_in = True
+
+        # 1) if NO USER is logged in, only shows VERIFIED, UNVERIFIED or no status content
+        # 2) if USER IS LOGGED IN, show:
+        # 2.1) user's contribution regardless the status
+        # 2.2) community_only content from user's communities. Rules:
+        # 2.2.1) is NOT COMMUNITY ONLY (False or NULL) but status is VERIFIED, UNVERIFIED or NULL
+        # 2.2.2) is COMMUNITY ONLY
+        # 2.3) everything from where user is Administrator (language/community pair)
+
+        if user_logged_in:
+
+            # 2.1) user's contribution regardless the status
+            queryset_user = queryset.filter(creator__id=request.user.id)
+
+            # 2.2) community_only content from user's communities
+            user_communities = CommunityMember.objects.filter(
+                user__id=int(request.user.id)
+            ).filter(
+                status__exact=CommunityMember.VERIFIED
+            ).values('community')
+
+            # 2.2.1) is NOT COMMUNITY ONLY (False or NULL) but status is VERIFIED, UNVERIFIED or NULL
+            # 2.2.2) is COMMUNITY ONLY
+            queryset_community = queryset.filter(
+                Q(community_only=False, status__exact=PlaceName.VERIFIED)
+                | Q(community_only=False, status__exact=PlaceName.UNVERIFIED)
+                | Q(community_only=False, status__isnull=True)
+                | Q(community_only__isnull=True, status__exact=PlaceName.VERIFIED)
+                | Q(community_only__isnull=True, status__exact=PlaceName.UNVERIFIED)
+                | Q(community_only__isnull=True, status__isnull=True)
+                | Q(community__in=user_communities)
+            )
+
+            # 2.3) everything from where user is Administrator (language/community pair)
+            admin_languages = Administrator.objects.filter(
+                user__id=int(request.user.id)).values('language')
+            admin_communities = Administrator.objects.filter(
+                user__id=int(request.user.id)).values('community')
+
+            if admin_languages and admin_communities:
+                # Filter PlaceNames by admin's languages
+                queryset_admin = queryset.filter(
+                    language__in=admin_languages, community__in=admin_communities
+                )
+                if queryset_admin:
+                    queryset = queryset_user.union(
+                        queryset_community).union(queryset_admin)
+                else:
+                    queryset = queryset_user.union(queryset_community)
+            else:  # user is not Administrator of anything
+                queryset = queryset_user.union(queryset_community)
+
+        else:  # no user is logged in
+            queryset = queryset.filter(
+                Q(status__exact=PlaceName.VERIFIED)
+                | Q(status__exact=PlaceName.UNVERIFIED)
+                | Q(status__isnull=True)
+            )
+
+        serializer = self.serializer_class(queryset, many=True)
+        return Response(serializer.data)
 
 
 # GEO LIST APIVIEWS
