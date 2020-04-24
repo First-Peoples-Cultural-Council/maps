a82d612bb398a07429df3ee0752ca64bdd24f125
Justin Carretas <justin@countable.ca>
2020-04-25 06:32:29 +0800

    Update - Trigger invite flow on admin command

--- web/users/management/commands/send_claim_profile_invites.py
+++ web/users/management/commands/send_claim_profile_invites.py
@@ -1,12 +1,9 @@
 from django.core.management.base import BaseCommand
+from users.notifications import send_claim_profile_invites
 
 
 class Command(BaseCommand):
     help = "Send out emails to registered users to claim their artist profiles."
 
     def handle(self, *args, **options):
-        send_invites()
-
-
-def send_invites():
-    print('SENDING NOTIFICATIONS')
+        send_claim_profile_invites()
