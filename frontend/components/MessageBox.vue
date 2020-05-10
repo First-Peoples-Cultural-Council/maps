fbe58d07482c7eacaba22a787616b74d773eac60
Mico Ang <mico@countable.ca>
2020-05-11 02:43:41 +0800

    [update] Added MessageBox component for popups

---
+++ frontend/components/MessageBox.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <b-modal v-model="show" ok-only hide-header @ok="toggleModal">
+      <div class="message-box-container">
+        <!-- <img class="message-box-icon" src="@/assets/images/user_icon.svg" /> -->
+        <span class="message-box-content">
+          {{ message }}
+        </span>
+      </div>
+    </b-modal>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    show: {
+      type: Boolean,
+      default: false
+    },
+    message: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+      modalShow: this.show
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.modalShow = false
+    }
+  }
+}
+</script>
+<style>
+.message-box-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
