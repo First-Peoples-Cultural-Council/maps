28af6feb5d349b1cab3139cd2fb4a80dd498479a
Justin Carretas <justin@countable.ca>
2020-05-12 11:53:47 +0800

    Update - Updated Notification format and functions

--- web/users/notifications.py
+++ web/users/notifications.py
@@ -1,12 +1,13 @@
+import os
+import re
+import hashlib
+import uuid
+
 from django.conf import settings
 from django.core.mail import send_mail
 
 from users.models import User, ProfileClaimRecord
 from language.models import RelatedData
-
-import re
-import hashlib
-import uuid
 
 
 def _format_fpcc(s):
@@ -22,85 +23,72 @@ def _format_fpcc(s):
 
 
 # email data refers to RelatedData with data_type = 'email'
-def send_claim_profile_invite(user_email, email_data):
+def send_claim_profile_invite(email):
     """
     Triggers Mail Sending
     """
-    salt = uuid.uuid4().hex.encode('utf-8')
-    encoded_email = email_data.value.encode('utf-8')
-    key = hashlib.sha512(encoded_email + salt).hexdigest()
+    salt = os.environ['INVITE_SALT'].encode('utf-8')
+    encoded_email = email.encode('utf-8')
+    key = hashlib.sha256(salt + encoded_email).hexdigest()
+
+    email_data = RelatedData.objects.filter(data_type='email', value=email)
+
+    profile_links = ''
+    for data in email_data:
+        profile_links += """<p>&emsp;<a href='{host}/art/{profile}' target='_blank'>{host}/art/{profile}</a></p>""".format(
+            host=settings.HOST,
+            profile=_format_fpcc(data.placename.name),
+        )
 
     message = """
         <h3>Greetings from First People's Cultural Council!</h3>
-        <p>We have recently migrated our data from <a href='{fp_artsmap_website}' target='_blank'>{fp_artsmap_website}</a> over to <a href='{fpcc_website}' target='_blank'>{fpcc_website}</a> and would like to invite you to claim your profile:</p>
-        <p>&emsp;<a href='{host}/art/{profile}' target='_blank'>{host}/art/{profile}</a></p><br/>
+        <p>We have recently migrated our data from <a href='{fp_artsmap_website}' target='_blank'>{fp_artsmap_website}</a> over to <a href='{fpcc_website}' target='_blank'>{fpcc_website}</a> and would like to invite you to claim your profile(s):</p>
+        {profile_links}<br/>
         <h4>Claim Your Profile</h4>
-        <p>If this profile is yours, please click on the link below to claim it:</p>
-        <p>&emsp;<a href='{host}/api/invite/confirm/{email}/{key}' target='_blank'>Claim Profile</a></p>
+        <p>If you own the following profile(s), please click on the link below to claim it:</p>
+        <p>&emsp;<a href='{host}/claim?email={email}&key={key}' target='_blank'>Claim Profile</a></p>
         <p>If it isn't please report that your email is being used by another person on our website by sending an email to <a href='mailto:{fpcc_email}' target='_blank'>{fpcc_email}</a>.</p><br/>
         <p>Thank you very much, and have a good day!</p>
     """.format(
         fp_artsmap_website='https://fp-artsmap.ca',
         fpcc_website='https://maps.fpcc.ca',
+        profile_links=profile_links,
         host=settings.HOST,
-        profile=_format_fpcc(email_data.placename.name),
         fpcc_email='info@fpcc.ca',
-        email=email_data.value,
+        email=email,
         key=key
     )
 
     # Send out the message
     send_mail(
-        subject="Claim FPCC Profile: %s" % email_data.placename.name,
+        subject="Claim Your FPCC Profile",
         message=message,
         from_email="info@fpcc.ca",
-        recipient_list=[email_data.value],
+        recipient_list=[email],
         html_message=message,
     )
-
-    # Update key and user email for record if it already exists
-    try:
-        record = ProfileClaimRecord.objects.get(profile_email=email_data.value)
-        record.key = key
-        record.user_email = user_email
-        record.save()
-    except ProfileClaimRecord.DoesNotExist:
-        ProfileClaimRecord.objects.create(
-            profile_email=email_data.value,
-            user_email=user_email,
-            key=key,
-            profile=email_data.placename
-        )        
 
 
 def send_claim_profile_invites():
     """
     Bulk Invite - Sends to every registered
     """
-    user_emails = list(User.objects.all().values_list('email', flat=True))
+    # This is actual data which we won't use yet
+    # emails = RelatedData.objects.exclude(value='').filter(
+    #     data_type='email').distinct('value').values_list('value', flat=True)
 
-    related_emails = RelatedData.objects.exclude(value='').filter(
-        data_type='email', value__in=user_emails)
+    # TESTING DATA START
+    emails = ['justin@countable.ca']  # Replace with your email
+    # TESTING DATA END
 
-    for email_data in related_emails:
+    for email in emails:
         # In the case of bulk sending, user_email = profile_email
-        send_claim_profile_invite(
-            user_email=email_data.value, email_data=email_data)
+        send_claim_profile_invite(email)
 
 
-def claim_profile(user_email, email_data):
+def claim_profile(email):
     """
     Claim profile - this allows claiming profile despite the user.email and the email not matching
     """
     # Send invite
-    send_claim_profile_invite(user_email, email_data)
-
-
-def resend_invite(user_email):
-    """
-    In case the email did not arrive, this function could be triggered
-    """
-    record = ProfileClaimRecord.objects.get(user_email=user_email)
-    email_data = RelatedData.objects.get(data_type='email', value=record.profile_email)
-
-    send_claim_profile_invite(user_email, email_data)
+    send_claim_profile_invite(email)
