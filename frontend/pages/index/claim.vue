2adff3ced5a15cab6d9e11f9296f4746107ae31a
Justin Carretas <justin@countable.ca>
2020-05-12 12:04:11 +0800

    Update - Added a page for claiming profile

---
+++ frontend/pages/index/claim.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="clain-email">
+    <div>
+      <Logo />
+    </div>
+    <div v-if="isLoggedIn && claimSuccess" class="claim-info-container">
+      <div class="alert alert-success">
+        {{ message }}
+      </div>
+    </div>
+    <div v-else class="claim-info-container no-user">
+      <div class="alert alert-danger">
+        {{ message }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as Cookies from 'js-cookie'
+import { getApiUrl, getCookie } from '@/plugins/utils.js'
+import Logo from '@/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  },
+  data() {
+    return {
+      claimSuccess: false,
+      message: 'Please wait while we are checking your User Invitation',
+      invalidMessage:
+        'Sorry but the URL link you provided to claim your profile is invalid',
+      redirectMessage: 'Please wait while we redirect you to the singup page',
+      createProfileMessage:
+        'Please wait while we connect your artist profile to your account',
+      claimSuccessMessage: 'Thank you for claiming your profile(s)'
+    }
+  },
+  computed: {
+    isLoggedIn() {
+      return this.$store.state.user.isLoggedIn
+    },
+    userid() {
+      return this.$store.state.user.user.id
+    },
+    email() {
+      return this.$store.state.user.user && this.$store.state.user.user.email
+    },
+    user() {
+      return this.$store.state.user.user
+    }
+  },
+  mounted() {
+    const inviteMode = Cookies.get('inviteMode')
+
+    const email = this.$route.query.email
+    const key = this.$route.query.key
+
+    // NOTE: Invite mode means you have a valid URL and you were asked to sign up
+    if (inviteMode && this.isLoggedIn) {
+      // The `if` block will be executed if the user is undergoing a claiming
+      // artist profile process. The condition is set in order to avoid
+      // the claim function to be triggered outside the invite flow
+      this.claimSuccess = true
+
+      this.message = this.createProfileMessage
+
+      this.claimArtistProfile(email, key)
+    } else if (!this.isLoggedIn) {
+      // If you are neither in invite mode, nor logged in, you will trigger the invite
+      // process and you will be asked to register. The Inviet Mode will be activated
+      if (email && key) {
+        this.processInvite(email, key)
+      } else {
+        this.message = this.invalidMessage
+      }
+    } else {
+      // If you access then claim page while you are logged in, but you're not actively
+      // claiming a profile, you will be notified that your claim URL is invalid
+      this.message = this.invalidMessage
+    }
+  },
+  methods: {
+    redirectLogin() {
+      window.location.href = `${process.env.COGNITO_URL}/login?response_type=token&client_id=${process.env.COGNITO_APP_CLIENT_ID}&redirect_uri=${process.env.COGNITO_HOST}`
+    },
+    async validateUrl(email, key, headers = {}) {
+      console.log(headers)
+
+      // Checking the validity will confirm if the invite URL was sent
+      // out by our system, and this would allow visual feedback to users
+      const validateUrl = `${getApiUrl('key/validate/')}`
+      const isValid = await this.$axios.post(
+        validateUrl,
+        {
+          email,
+          key
+        },
+        {
+          headers
+        }
+      )
+
+      return isValid
+    },
+    async processInvite(email, key) {
+      const result = await this.validateUrl(email, key)
+      const isValid = result.data.valid
+
+      if (isValid) {
+        // Set cookies that will be reused later on after logging in
+        Cookies.set('inviteEmail', email)
+        Cookies.set('inviteKey', key)
+        Cookies.set('inviteMode', true)
+
+        this.message = this.redirectMessage
+
+        this.redirectLogin()
+      } else {
+        this.message = this.invalidMessage
+      }
+    },
+    async claimArtistProfile(email, key) {
+      const headers = {
+        'X-CSRFToken': getCookie('csrftoken')
+      }
+      console.log(headers)
+      const result = await this.validateUrl(email, key, headers)
+      const isValid = result.data.valid
+
+      if (isValid) {
+        this.message = this.claimSuccessMessage
+
+        // Cookies.remove('inviteEmail')
+        // Cookies.remove('inviteKey')
+        // Cookies.remove('inviteMode')
+      } else {
+        this.message = this.invalidMessage
+      }
+    }
+  }
+}
+</script>
+
+<style>
+.clain-email {
+  height: 100%;
+}
+.claim-info-container {
+  padding: 16px;
+  font-family: sans-serif;
+  height: 100%;
+}
+.no-user {
+  background-color: rgba(0, 0, 0, 0.5);
+}
+</style>
