fd6caf712fcea923101bc265f36630e1e43478a8
Mico Ang <mico@countable.ca>
2020-06-05 03:35:08 +0800

    [update] added function for verifiying, and getting youtube id

--- frontend/plugins/utils.js
+++ frontend/plugins/utils.js
@@ -267,4 +267,18 @@ export const isValidURL = email => {
   return !!regex.test(email.toLowerCase());
 }
 
+export const isValidYoutubeLink = url => {
+  const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/
+      const match = url.match(regExp)
+      return match && match[7].length === 11 ? match[7] : false
+}
+export const getYoutubeThumbnail = url => {
+
+  return `https://img.youtube.com/vi/${isValidYoutubeLink(
+        url
+      )}/hqdefault.jpg`
+}
+
+
+
 
