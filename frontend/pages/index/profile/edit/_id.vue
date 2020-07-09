6cbc35f64270a914d169ad8951a1e273e58f468e
Mico Ang <mico@countable.ca>
2020-07-09 22:57:25 +0800

    [update] fixed profile edit mobile slider view

--- frontend/pages/index/profile/edit/_id.vue
+++ frontend/pages/index/profile/edit/_id.vue
@@ -1,192 +1,193 @@
 <template>
   <div>
     <div v-if="isCurrentUser">
-      <client-only>
+      <!-- <client-only> -->
+      <div
+        v-if="!mobileContent"
+        class="content-collapse d-none content-mobile-title"
+      >
         <div>
+          User: <b-badge variant="primary">{{ getUserName() }}</b-badge
+          ><b-badge class="ml-2" variant="primary">Expand To Edit</b-badge>
+        </div>
+        <div
+          class="content-collapse-btn"
+          @click="$store.commit('sidebar/setMobileContent', true)"
+        >
+          <img src="@/assets/images/arrow_up_icon.svg" />
+        </div>
+      </div>
+      <div>
+        <div class="hide-mobile" :class="{ 'content-mobile': mobileContent }">
+          <Logo :logo-alt="2" class="pt-2 pb-2 hide-mobile"></Logo>
           <div
-            v-if="!mobileContent"
-            class="content-collapse d-none content-mobile-title"
+            class="text-center d-none mobile-close"
+            :class="{ 'content-mobile': mobileContent }"
+            @click="$store.commit('sidebar/setMobileContent', false)"
           >
-            <div>
-              User: <b-badge variant="primary">{{ getUserName() }}</b-badge
-              ><b-badge class="ml-2" variant="primary">Expand To Edit</b-badge>
-            </div>
-            <div
-              class="content-collapse-btn"
-              @click="$store.commit('sidebar/setMobileContent', true)"
-            >
-              <img src="@/assets/images/arrow_up_icon.svg" />
-            </div>
+            <img
+              class="d-inline-block"
+              src="@/assets/images/arrow_down_icon.svg"
+            />
           </div>
-          <div class="hide-mobile" :class="{ 'content-mobile': mobileContent }">
-            <Logo :logo-alt="2" class="pt-2 pb-2 hide-mobile"></Logo>
-            <div
-              class="text-center d-none mobile-close"
-              :class="{ 'content-mobile': mobileContent }"
-              @click="$store.commit('sidebar/setMobileContent', false)"
-            >
-              <img
-                class="d-inline-block"
-                src="@/assets/images/arrow_down_icon.svg"
-              />
+
+          <h4 class="profile-title pt-3 pb-3 color-gray">
+            <h4 class="text-uppercase contribute-title mr-2">
+              Edit Profile
+            </h4>
+          </h4>
+
+          <section class="pl-3 pr-3">
+            <div class="upload-img-container mt-3">
+              <div class="upload-img">
+                <img
+                  v-if="fileSrc === null"
+                  class="upload-placeholder"
+                  :src="thumbnailPlaceholder()"
+                />
+                <img v-else :src="fileSrc" />
+                <b-button
+                  v-if="fileSrc !== null"
+                  class="upload-remove"
+                  @click="removeImg()"
+                  >Remove Image</b-button
+                >
+              </div>
+
+              <b-form-file
+                ref="fileUpload"
+                v-model="fileImg"
+                class="file-upload-input mt-2"
+                :placeholder="filePlaceholder()"
+                drop-placeholder="Drop file here..."
+                accept="image/*"
+              ></b-form-file>
             </div>
 
-            <h4 class="profile-title pt-3 pb-3 color-gray">
-              <h4 class="text-uppercase contribute-title mr-2">
-                Edit Profile
-              </h4>
-            </h4>
+            <label
+              for="firstname"
+              class="contribute-title-one mt-3 mb-1 color-gray font-weight-bold font-09"
+              >First Name</label
+            >
+            <b-form-input
+              id="firstname"
+              v-model="user.first_name"
+              type="text"
+              class="font-08"
+            ></b-form-input>
 
-            <section class="pl-3 pr-3">
-              <div class="upload-img-container mt-3">
-                <div class="upload-img">
-                  <img
-                    v-if="fileSrc === null"
-                    class="upload-placeholder"
-                    :src="thumbnailPlaceholder()"
-                  />
-                  <img v-else :src="fileSrc" />
-                  <b-button
-                    v-if="fileSrc !== null"
-                    class="upload-remove"
-                    @click="removeImg()"
-                    >Remove Image</b-button
-                  >
-                </div>
+            <label
+              for="lastname"
+              class="contribute-title-one mt-3 mb-1 color-gray font-weight-bold font-09"
+              >Last Name</label
+            >
+            <b-form-input
+              id="lastname"
+              v-model="user.last_name"
+              type="text"
+              class="font-08"
+            ></b-form-input>
 
-                <b-form-file
-                  ref="fileUpload"
-                  v-model="fileImg"
-                  class="file-upload-input mt-2"
-                  :placeholder="filePlaceholder()"
-                  drop-placeholder="Drop file here..."
-                  accept="image/*"
-                ></b-form-file>
+            <label
+              class="contribute-title-one mt-3 mb-1 color-gray font-weight-bold font-09"
+              >Languages</label
+            >
+            <multiselect
+              v-model="value"
+              placeholder="Search or select a language"
+              label="name"
+              track-by="id"
+              :options="options"
+              :multiple="true"
+            ></multiselect>
+
+            <div v-if="isNonBCLanguage" class="website-container mt-3">
+              <div>
+                <label class="contribute-title-one">Non B.C. Language</label>
               </div>
 
-              <label
-                for="firstname"
-                class="contribute-title-one mt-3 mb-1 color-gray font-weight-bold font-09"
-                >First Name</label
+              <div
+                v-for="(lang, index) in languageNonBC"
+                :key="index"
+                class="site-input-container"
               >
-              <b-form-input
-                id="firstname"
-                v-model="user.first_name"
-                type="text"
-                class="font-08"
-              ></b-form-input>
-
-              <label
-                for="lastname"
-                class="contribute-title-one mt-3 mb-1 color-gray font-weight-bold font-09"
-                >Last Name</label
-              >
-              <b-form-input
-                id="lastname"
-                v-model="user.last_name"
-                type="text"
-                class="font-08"
-              ></b-form-input>
-
-              <label
-                class="contribute-title-one mt-3 mb-1 color-gray font-weight-bold font-09"
-                >Languages</label
-              >
-              <multiselect
-                v-model="value"
-                placeholder="Search or select a language"
-                label="name"
-                track-by="id"
-                :options="options"
-                :multiple="true"
-              ></multiselect>
-
-              <div v-if="isNonBCLanguage" class="website-container mt-3">
-                <div>
-                  <label class="contribute-title-one">Non B.C. Language</label>
-                </div>
-
-                <div
-                  v-for="(lang, index) in languageNonBC"
-                  :key="index"
-                  class="site-input-container"
+                <b-form-input
+                  :id="`language-${lang}`"
+                  v-model="lang.value"
+                  type="text"
+                  placeholder="(ex. Spanish, English, etc.)"
+                ></b-form-input>
+                <span
+                  v-if="
+                    (index !== 0 && languageNonBC.length !== 1) ||
+                      languageNonBC.length > 1
+                  "
+                  class="site-btn"
+                  @click="removeLanguage(index)"
+                  >-</span
                 >
-                  <b-form-input
-                    :id="`language-${lang}`"
-                    v-model="lang.value"
-                    type="text"
-                    placeholder="(ex. Spanish, English, etc.)"
-                  ></b-form-input>
-                  <span
-                    v-if="
-                      (index !== 0 && languageNonBC.length !== 1) ||
-                        languageNonBC.length > 1
-                    "
-                    class="site-btn"
-                    @click="removeLanguage(index)"
-                    >-</span
-                  >
-                  <span
-                    v-if="index + 1 === languageNonBC.length"
-                    class="site-btn"
-                    @click="addLanguage()"
-                    >+</span
-                  >
-                </div>
+                <span
+                  v-if="index + 1 === languageNonBC.length"
+                  class="site-btn"
+                  @click="addLanguage()"
+                  >+</span
+                >
               </div>
+            </div>
 
+            <label
+              class="color-gray font-weight-bold contribute-title-one mb-1 mt-4 font-09"
+              >Community</label
+            >
+            <multiselect
+              v-model="community"
+              placeholder="Search or select a community"
+              label="name"
+              track-by="id"
+              :options="communities"
+            ></multiselect>
+
+            <label
+              class="contribute-title-one mt-3 color-gray font-weight-bold font-09 mb-2"
+              >User Description</label
+            >
+
+            <section class="pl-3 pr-3 pb-2">
+              <div class="pl-3 pr-3">
+                <div id="quill" ref="quill"></div>
+              </div>
               <label
                 class="color-gray font-weight-bold contribute-title-one mb-1 mt-4 font-09"
-                >Community</label
+                >Notifications</label
               >
-              <multiselect
-                v-model="community"
-                placeholder="Search or select a community"
-                label="name"
-                track-by="id"
-                :options="communities"
-              ></multiselect>
+              <b-form-select
+                v-model="user.notification_frequency"
+                :options="notification_options"
+              ></b-form-select>
 
-              <label
-                class="contribute-title-one mt-3 color-gray font-weight-bold font-09 mb-2"
-                >User Description</label
+              <b-alert v-if="errors.length" show variant="warning" dismissible>
+                <ul>
+                  <li v-for="err in errors" :key="err">{{ err }}</li>
+                </ul>
+              </b-alert>
+              <b-button
+                class="btn btn-primary mt-4"
+                variant="primary"
+                @click="save"
+                >Save</b-button
               >
+              <b-button
+                class="btn btn-primary mt-4"
+                variant="danger"
+                @click="handleCancel"
+              >
+                Cancel
+              </b-button>
             </section>
-          </div>
+          </section>
         </div>
-      </client-only>
-      <div class="pl-3 pr-3">
-        <div id="quill" ref="quill"></div>
       </div>
-
-      <client-only>
-        <section class="pl-3 pr-3 pb-2">
-          <label
-            class="color-gray font-weight-bold contribute-title-one mb-1 mt-4 font-09"
-            >Notifications</label
-          >
-          <b-form-select
-            v-model="user.notification_frequency"
-            :options="notification_options"
-          ></b-form-select>
-
-          <b-alert v-if="errors.length" show variant="warning" dismissible>
-            <ul>
-              <li v-for="err in errors" :key="err">{{ err }}</li>
-            </ul>
-          </b-alert>
-          <b-button class="btn btn-primary mt-4" variant="primary" @click="save"
-            >Save</b-button
-          >
-          <b-button
-            class="btn btn-primary mt-4"
-            variant="danger"
-            @click="handleCancel"
-          >
-            Cancel
-          </b-button>
-        </section>
-      </client-only>
+      <!-- </client-only> -->
     </div>
     <ErrorScreen v-else></ErrorScreen>
   </div>
@@ -372,7 +373,9 @@ export default {
     },
     getUserName() {
       return (
-        this.user && (this.user.first_name || this.user.username.split('__')[0])
+        this.user &&
+        (`${this.user.first_name} ${this.user.last_name}` ||
+          this.user.username.split('__')[0])
       )
     },
     removeImg() {
