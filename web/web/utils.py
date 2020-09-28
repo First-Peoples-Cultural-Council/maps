59663a461c274646d4754e7fe44e9ae7e587a412
Justin Carretas <justin@countable.ca>
2020-09-28 13:20:27 +0800

    [UPDATE] Secure APIs for Community

--- web/web/utils.py
+++ web/web/utils.py
@@ -1,3 +1,4 @@
+from users.models import Administrator
 
 def is_user_permitted(request, pk_to_compare):
     """
@@ -5,6 +6,15 @@ def is_user_permitted(request, pk_to_compare):
     """
     if request and hasattr(request, "user"):
         if request.user.is_authenticated and request.user.id == int(pk_to_compare):
+            return True
+    
+    return False
+
+def is_user_community_admin(request, community):
+    community_admins = list(Administrator.objects.filter(community=community).values_list('user', flat=True))
+    
+    if request and hasattr(request, "user"):
+        if request.user.is_staff or request.user.id in community_admins:
             return True
     
     return False
