274a24aff5afb5bbcb5c4fbae752bf775bf22564
Mico Ang <mico@countable.ca>
2020-10-03 20:25:35 +0800

    [update] if progress function is not present, dont show the progress bar

--- frontend/store/file.js
+++ frontend/store/file.js
@@ -15,22 +15,26 @@ export const mutations = {
 }
 
 export const actions = {
-  async uploadMedia({ commit }, formData) {
-    console.log('enter here')
-
+  async uploadMedia({ commit }, dataObj) {
     const config = {
       headers: { 'X-CSRFToken': getCookie('csrftoken') },
       onUploadProgress: progressEvent => {
-        const percentCompleted = Math.round(
-          (progressEvent.loaded * 100) / progressEvent.total
-        )
-        this.$root.$emit('initiateLoadingModal')
-        console.log(percentCompleted)
+        const { loaded, total } = progressEvent
+        const percentCompleted = Math.round((loaded * 100) / total)
+        console.log(`${loaded}KB uploaded of ${total}KB`)
+
+        if (dataObj.callProgressModal) {
+          dataObj.callProgressModal(percentCompleted)
+        }
       }
     }
 
     try {
-      const result = await this.$axios.post(`/api/media/`, formData, config)
+      const result = await this.$axios.post(
+        `/api/media/`,
+        dataObj.formData,
+        config
+      )
       return result
     } catch (e) {
       return { error: e, status: 'failed' }
