902feb2240a7cf0f4d7c06979db98783b3788977
Miracleo Jean Sarmiento <miracleo@countable.ca>
2020-10-16 12:17:24 +0800

    [UPDATE] Added API to get categories for grants

--- web/grants/views/grant.py
+++ web/grants/views/grant.py
@@ -1,10 +1,17 @@
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
-from rest_framework import mixins
+from rest_framework import mixins, views
+from rest_framework.response import Response
 
 from grants.models import Grant
 from grants.serializers import GrantSerializer, GrantDetailSerializer
 from .base import BaseGenericViewSet
+
+CATEGORIES = {
+    'A': ['ARTS', 'AIND', 'ASHR', 'AORG', 'AADM', 'AMIC', 'ALND', 'AEMIP', 'AECMR', 'ATPMP'],
+    'H': ['HMIC', 'HSOP'], 
+    'L': ['LALI', 'LBCLI', 'LDIGI', 'LFV', 'LILG', 'LANG', 'LLN', 'LLRPP', 'LMAP', 'LPATH', 'LRML', 'LTECH', 'LYES']
+}
 
 
 class GrantViewSet(mixins.ListModelMixin,
@@ -18,3 +25,33 @@ class GrantViewSet(mixins.ListModelMixin,
     @method_decorator(never_cache)
     def list(self, request):
         return super().list(request)
+
+
+class GrantCategoryAPIView(views.APIView):
+    def get(self, request):
+        def get_parent_category(category):
+            parent_category = None
+            for k, v in CATEGORIES.items():
+                if category in v:
+                    parent_category = k
+                    break
+            return parent_category
+
+        parent_categories = list(CATEGORIES.keys())
+        categories_data = []
+        categories = Grant.objects.order_by('category').values_list('category', flat=True).distinct()
+
+        # start pk for subcategories after parent categories
+        pk = len(parent_categories) + 1
+        for category in categories:
+            parent_category = get_parent_category(category)
+            data = {
+                'id': pk,
+                'name': category,
+                'order': CATEGORIES[parent_category].index(category) + 1 if parent_category else None,
+                'parent': parent_categories.index(parent_category) + 1 if parent_category else None
+            }
+            pk += 1
+            categories_data.append(data)
+
+        return Response(categories_data)
