1dcbf23d0f068000a2cd4f80680ba82dcd03b891
Miracleo Jean Sarmiento <miracleo@countable.ca>
2020-10-23 00:20:18 +0800

    [UPDATE] Updated category field value in serializers for grants

--- web/grants/serializers.py
+++ web/grants/serializers.py
@@ -5,6 +5,7 @@ from .models import Grant, GrantCategory
 
 
 class GrantSerializer(GeoFeatureModelSerializer):
+    category = serializers.CharField(source="grant_category.name", allow_null=True)
 
     class Meta:
         model = Grant
@@ -14,10 +15,11 @@ class GrantSerializer(GeoFeatureModelSerializer):
 
 
 class GrantDetailSerializer(serializers.ModelSerializer):
+    category = serializers.CharField(source='grant_category.name', allow_null=True)
 
     class Meta:
         model = Grant
-        fields = "__all__"
+        exclude = ("grant_category",)
         geo_field = "point"
 
 
