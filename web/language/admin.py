3d17d6bdc54208a7b1243eb7bfec184fb01d9918
Justin Carretas <justin@countable.ca>
2023-09-07 03:43:53 +0800

    Created a reusable widget that accepts a FeatureCollection for the geom fields

--- web/language/admin.py
+++ web/language/admin.py
@@ -20,9 +20,10 @@ from .models import (
     Recording,
     Taxonomy,
     PlaceNameTaxonomy,
-    RelatedData
+    RelatedData,
 )
-from .widgets import LatLongWidget
+from .widgets import LatLongWidget, GeoJSONFeatureCollectionWidget
+
 
 # INLINES
 class DialectInline(admin.TabularInline):
@@ -44,24 +45,19 @@ class RelatedDataInline(admin.TabularInline):
 # ADMINS
 class LanguageAdmin(admin.ModelAdmin):
     list_display = ("name", "sleeping", "family")
-    exclude = ("audio_file",)
-    search_fields = (
-        "name",
-        "family__name"
-    )
-    inlines = [
-        DialectInline,
-        LanguageLinkInline
-    ]
+    exclude = ("audio_file", "bbox")
+    search_fields = ("name", "family__name")
+    inlines = [DialectInline, LanguageLinkInline]
+    formfield_overrides = {
+        geomodels.PolygonField: {"widget": GeoJSONFeatureCollectionWidget},
+    }
 
 
 class CommunityAdmin(admin.ModelAdmin):
     exclude = ("audio_file",)
-    search_fields = (
-        "name",
-    )
+    search_fields = ("name",)
     formfield_overrides = {
-        geomodels.PointField: {'widget': LatLongWidget},
+        geomodels.PointField: {"widget": LatLongWidget},
     }
     inlines = [
         CommunityLinkInline,
@@ -70,17 +66,12 @@ class CommunityAdmin(admin.ModelAdmin):
 
 class DialectAdmin(admin.ModelAdmin):
     list_display = ("name", "language")
-    search_fields = (
-        "name",
-        "language__name"
-    )
+    search_fields = ("name", "language__name")
 
 
 class LNADataAdmin(admin.ModelAdmin):
     list_display = ("name", "fluent_speakers")
-    search_fields = (
-        "name",
-    )
+    search_fields = ("name",)
 
 
 class RelatedDataAdmin(admin.ModelAdmin):
@@ -90,24 +81,30 @@ class RelatedDataAdmin(admin.ModelAdmin):
         "data_type",
         "value",
         "placename__name",
-        "placename__kind"
+        "placename__kind",
     )
 
 
 class PlaceNameAdminForm(forms.ModelForm):
     class Meta:
         model = PlaceName
-        fields = '__all__'
+        fields = "__all__"
+        widgets = {
+            "geom": GeoJSONFeatureCollectionWidget(),
+        }
 
     def clean(self):
         cleaned_data = super().clean()
+        kind = cleaned_data.get("kind")
         communities = cleaned_data.get("communities")
         other_community = cleaned_data.get("other_community")
 
-        if not communities and not other_community:
-            raise forms.ValidationError({
-                'communities': 'Either `Communities` or `Other community` is required.'
-            })
+        if kind not in ["", "poi"] and not communities and not other_community:
+            raise forms.ValidationError(
+                {
+                    "communities": "Either `Communities` or `Other community` is required."
+                }
+            )
 
         return cleaned_data
 
@@ -134,18 +131,12 @@ class PlaceNameAdmin(admin.ModelAdmin):
 class MediaAdmin(admin.ModelAdmin):
     list_display = ("name", "file_type", "media_file", "url")
     readonly_fields = ("created",)
-    search_fields = (
-        "name",
-        "file_type"
-    )
+    search_fields = ("name", "file_type")
 
 
 class TaxonomyAdmin(admin.ModelAdmin):
     list_display = ("name", "parent")
-    search_fields = (
-        "name",
-        "parent__name"
-    )
+    search_fields = ("name", "parent__name")
 
 
 class PlaceNameTaxonomyAdmin(admin.ModelAdmin):
