68a3335e9b5d4614246b4f9c2755ee21b65c9d62
Justin Carretas <justin@countable.ca>
2023-11-17 13:44:38 +0800

    Added command to update community language stats

--- web/language/management/commands/update_language_stats.py
+++ web/language/management/commands/update_language_stats.py
@@ -7,9 +7,9 @@ from language.models import Language, Community, CommunityLanguageStats
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        filename = options["filename"]
-        check = options["check"] or 1
-        update_language_stats(filename, check)
+        filename = options.get("filename")
+        check = options.get("check", None)
+        update_language_stats_from_file(filename, check)
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -23,31 +23,96 @@ class Command(BaseCommand):
         )
 
 
-def update_language_stats(filename, check=1):
+def update_language_stats_from_file(filename, check=1):
     print("Reading ", filename)
     print("Check ", check)
 
     df = pd.read_csv(filename)
 
-    if check:
-        error = False
+    error = False
+    language_stats = []
 
-        for index, row in df.iterrows():
-            row_number = index + 1
-            current_community = row["Community"]
-            current_language = row["Language"]
+    for index, row in df.iterrows():
+        row_number = index + 2
+        current_community = row["Community"]
+        current_language = row["Language"]
+        total_population = row["Population"]
+        fluent_speakers = row["Fluent Speakers"]
+        semi_speakers = row["Semi-Speakers"]
+        active_learners = row["Learners"]
 
-            try:
-                Community.objects.get(name__unaccent__iexact=current_community)
-            except Community.DoesNotExist:
-                error = True
-                print(f"Row {row_number} - Community not found: {current_community}")
+        community = None
+        language = None
 
-            try:
-                Language.objects.get(name__unaccent__iexact=current_language)
-            except Language.DoesNotExist:
-                error = True
-                print(f"Row {row_number} - Language not found: {current_language}")
+        try:
+            community = Community.objects.get(name__unaccent__iexact=current_community)
+        except Community.DoesNotExist:
+            error = True
+            print(f"Row {row_number} - Community not found: {current_community}")
 
-        if not error:
-            print("File check passed")
+        try:
+            language = Language.objects.get(name__unaccent__iexact=current_language)
+        except Language.DoesNotExist:
+            error = True
+            print(f"Row {row_number} - Language not found: {current_language}")
+
+        if community and language:
+            language_stats.append(
+                {
+                    "community": community.id,
+                    "language": language.id,
+                    "total_population": total_population,
+                    "fluent_speakers": fluent_speakers,
+                    "semi_speakers": semi_speakers,
+                    "active_learners": active_learners,
+                }
+            )
+
+    if check and error:
+        return print("File check failed, upload will not proceed")
+
+    print("Uploading")
+
+    for data in language_stats:
+        community_id = data.get("community")
+        language_id = data.get("language")
+        total_population = data.get("total_population")
+        fluent_speakers = data.get("fluent_speakers")
+        semi_speakers = data.get("semi_speakers")
+        active_learners = data.get("active_learners")
+
+        latest_stats_for_pair = None
+        stats_to_delete = None
+
+        community = Community.objects.get(pk=community_id)
+        language = Language.objects.get(pk=language_id)
+
+        # Delete all duplicates, if there are any
+        matching_stats = CommunityLanguageStats.objects.filter(
+            community=community,
+            language=language,
+        )
+
+        latest_stats_for_pair = matching_stats.last()
+
+        if latest_stats_for_pair:
+            stats_to_delete = matching_stats.exclude(id=latest_stats_for_pair.id)
+            stats_to_delete.delete()
+
+        # Save new data
+        stats, _created = CommunityLanguageStats.objects.get_or_create(
+            community=community,
+            language=language,
+        )
+
+        # Update community language stats
+        stats.fluent_speakers = fluent_speakers
+        stats.semi_speakers = semi_speakers
+        stats.active_learners = active_learners
+        stats.save()
+
+        # Update community population
+        community = stats.community
+        community.population = total_population
+        community.save()
+        print(f"Saved {stats.language.name} - {stats.community.name}")
