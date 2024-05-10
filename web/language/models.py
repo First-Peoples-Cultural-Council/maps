51e1e22daf531eab5b82ead581602385c305d041
Justin Carretas <justin@countable.ca>
2024-05-11 04:49:33 +0800

    Refactors and clean up

--- web/language/models.py
+++ web/language/models.py
@@ -10,10 +10,25 @@ from django.db.models.signals import post_save
 from django.core.mail import send_mail
 from django.contrib.postgres.fields import ArrayField
 
+from users.models import User
 from web.models import BaseModel, CulturalModel
 from web.utils import get_art_link, get_comm_link, get_place_link, get_admin_email_list
-from web.constants import *
-from users.models import User
+from web.constants import (
+    FLAGGED,
+    UNVERIFIED,
+    VERIFIED,
+    REJECTED,
+    STATUS_DISPLAY,
+    ROLE_ADMIN,
+    ROLE_MEMBER,
+    PUBLIC_ART,
+    ORGANIZATION,
+    ARTIST,
+    EVENT,
+    RESOURCE,
+    GRANT,
+    POI,
+)
 
 
 class LanguageFamily(BaseModel):
@@ -172,42 +187,41 @@ class CommunityMember(models.Model):
     verified_by = models.CharField(max_length=255, default="", null=True, blank=True)
     date_verified = models.DateField(null=True, blank=True)
 
-    class Meta:
-        unique_together = ("user", "community")
-
+    @staticmethod
     def create_member(user_id, community_id):
         member = CommunityMember()
         member.user = User.objects.get(pk=user_id)
         member.community = Community.objects.get(pk=community_id)
         member.status = UNVERIFIED
         member.save()
-
         return member
 
+    @staticmethod
     def member_exists(user_id, community_id):
-        member = CommunityMember.objects.filter(user__id=user_id).filter(
-            community__id=community_id
+        return (
+            CommunityMember.objects.filter(user__id=user_id)
+            .filter(community__id=community_id)
+            .exists()
         )
-        if member:
-            return True
-        else:
-            return False
 
-    def verify_member(id, admin):
-        member = CommunityMember.objects.get(pk=int(id))
+    @staticmethod
+    def verify_member(user_id, admin):
+        member = CommunityMember.objects.get(pk=int(user_id))
         member.status = VERIFIED
         member.verified_by = admin.get_full_name()
         member.date_verified = timezone.now()
         member.save()
 
-    def reject_member(id, admin):
-        member = CommunityMember.objects.get(pk=int(id))
+    @staticmethod
+    def reject_member(user_id, admin):
+        member = CommunityMember.objects.get(pk=int(user_id))
         member.status = REJECTED
         member.verified_by = admin.get_full_name()
         member.date_verified = timezone.now()
         member.save()
 
     class Meta:
+        unique_together = ("user", "community")
         verbose_name_plural = "Community Members"
 
 
@@ -334,7 +348,9 @@ class PlaceName(CulturalModel):
 
         if self.status in [REJECTED, FLAGGED]:
             message += "<p>Reason: {}</p>".format(self.status_reason)
-            message += "<p>Please apply the suggested changes and try to submit your contribution for evaluation again.</p>"
+            message += """
+                <p>Please apply the suggested changes and try to submit your contribution for evaluation again.</p>
+            """
 
         send_mail(
             subject,
@@ -367,11 +383,11 @@ class PlaceName(CulturalModel):
         admin_list = get_admin_email_list()
 
         formatted_kind = self.kind.upper().replace("_", " ")
-        page = (
-            get_place_link(self)
-            if self.kind == "" or self.kind == "poi"
-            else get_art_link(self)
-        )
+
+        if self.kind in ["", "poi"]:
+            page = get_place_link(self)
+        else:
+            page = get_art_link(self)
 
         message = """
             <h3>Greetings from First People's Cultural Council!</h3>
@@ -461,7 +477,9 @@ class Media(BaseModel):
 
         if self.status in [REJECTED, FLAGGED]:
             message += "<p>Reason: {}</p>".format(self.status_reason)
-            message += "<p>Please apply the suggested changes and try to submit your contribution for evaluation again.</p>"
+            message += """
+                <p>Please apply the suggested changes and try to submit your contribution for evaluation again.</p>
+            """
 
         send_mail(
             subject,
@@ -549,19 +567,21 @@ class Favourite(BaseModel):
     point = models.PointField(null=True, default=None)
     zoom = models.IntegerField(default=0)
 
+    @staticmethod
     def favourite_place_already_exists(user_id, place_id):
-        favourite = Favourite.objects.filter(user__id=user_id).filter(place_id=place_id)
-        if favourite:
-            return True
-        else:
-            return False
+        return (
+            Favourite.objects.filter(user__id=user_id)
+            .filter(place_id=place_id)
+            .exists()
+        )
 
+    @staticmethod
     def favourite_media_already_exists(user_id, media_id):
-        favourite = Favourite.objects.filter(user__id=user_id).filter(media_id=media_id)
-        if favourite:
-            return True
-        else:
-            return False
+        return (
+            Favourite.objects.filter(user__id=user_id)
+            .filter(media_id=media_id)
+            .exists()
+        )
 
 
 class Notification(BaseModel):
@@ -611,7 +631,10 @@ class Taxonomy(models.Model):
         default=None,
         null=True,
         blank=True,
-        help_text="Value that determines the ordering of taxonomy. The lower the value is, the higher it is on the list",
+        help_text=(
+            "Value that determines the ordering of taxonomy."
+            "The lower the value is, the higher it is on the list"
+        ),
     )
     parent = models.ForeignKey(
         "self",
@@ -684,6 +707,7 @@ def _get_claim_url(email):
     return f"{host}/claim?email={email}&key={key}"
 
 
+# pylint:disable=unused-argument
 def placename_post_save(sender, instance, created, **kwargs):
     placename = instance
 
@@ -691,6 +715,7 @@ def placename_post_save(sender, instance, created, **kwargs):
         placename.notify()
 
 
+# pylint:disable=unused-argument
 def media_post_save(sender, instance, created, **kwargs):
     media = instance
 
