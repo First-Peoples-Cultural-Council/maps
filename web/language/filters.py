104aa46b9f2dc68df41a7afbdb1835a991c56bc7
Justin Carretas <justin@countable.ca>
2024-08-10 05:57:08 +0800

    Clean up language/filters.py

--- web/language/filters.py
+++ web/language/filters.py
@@ -3,8 +3,9 @@ from django_filters import Filter
 
 class ListFilter(Filter):
     def filter(self, qs, value):
-        if value not in (None, ''):
-            strings = [v for v in value.split(',')]
-            print(strings)
-            return qs.filter(**{'%s__%s' % (self.field_name, self.lookup_expr): strings})
+        if value not in (None, ""):
+            strings = list(value.split(","))
+            return qs.filter(
+                **{"%s__%s" % (self.field_name, self.lookup_expr): strings}
+            )
         return qs
