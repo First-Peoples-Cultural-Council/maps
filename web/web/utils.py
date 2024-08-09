d8ed7dcf820fa58c34cb420b39b9ba439d094abe
Justin Carretas <justin@countable.ca>
2024-08-10 06:10:01 +0800

    Clean up web/utils.py

--- web/web/utils.py
+++ web/web/utils.py
@@ -48,28 +48,37 @@ def is_user_permitted(request, pk_to_compare):
     if request and hasattr(request, "user"):
         if request.user.is_authenticated and request.user.id == int(pk_to_compare):
             return True
-    
+
     return False
 
+
 def is_user_community_admin(request, community):
-    community_admins = list(Administrator.objects.filter(community=community).values_list('user', flat=True))
-    
+    community_admins = list(
+        Administrator.objects.filter(community=community).values_list("user", flat=True)
+    )
+
     if request and hasattr(request, "user"):
         if request.user.is_staff or request.user.id in community_admins:
             return True
-    
+
     return False
+
 
 def get_admin_email_list():
     # Return Test Email if in DEBUG mode [TODO: Make configurable in the future]
     if settings.DEBUG:
-        return ['justin@countable.ca']
+        return ["justin@countable.ca"]
 
     # FPCC ADMINS that are registered in the site and are also assigned a superuser status
-    registered_admins = list(User.objects.filter(
-        is_superuser=True,
-        email__isnull=False,
-    ).exclude(email='').values_list('email', flat=True) or [])
+    registered_admins = list(
+        User.objects.filter(
+            is_superuser=True,
+            email__isnull=False,
+        )
+        .exclude(email="")
+        .values_list("email", flat=True)
+        or []
+    )
 
     # FPCC ADMINS that don't necessarily need to be registered but are required to be notified
     fpcc_admins = [j[1] for j in settings.FPCC_ADMINS]
