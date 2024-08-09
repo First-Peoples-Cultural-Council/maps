78d03963c6141cc3a83dbd6d4e740d80438257ce
Justin Carretas <justin@countable.ca>
2024-08-10 06:38:55 +0800

    Clean up users/admin.py

--- web/users/admin.py
+++ web/users/admin.py
@@ -1,24 +1,19 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import User, Administrator
+from users.forms import CustomUserCreationForm, CustomUserChangeForm
+from users.models import User, Administrator
 
 
-class UserAdmin(UserAdmin):
+class UserAdmin(DjangoUserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    list_display = ("username", "email", "first_name",
-                    "last_name", "is_superuser")
+    list_display = ("username", "email", "first_name", "last_name", "is_superuser")
 
 
 class AdministratorAdmin(admin.ModelAdmin):
     list_display = ("user", "language", "community")
-    search_fields = (
-        "user",
-        "language",
-        "community"
-    )
+    search_fields = ("user", "language", "community")
 
 
 admin.site.register(User, UserAdmin)
