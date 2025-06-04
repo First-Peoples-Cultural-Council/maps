202281d520b1134d786acd016adea73c6d8f34b0
rohit-fpcc <rsharma@fpcc.ca>
2025-06-04 08:14:06 -0700

    Revert "Integrate matomo to the service to track increasing traffic"
    This reverts commit e033e388ac9fecd6042e8e50a4f0d2c5afc814cc.

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -9,12 +9,6 @@ module.exports = {
     host: '0.0.0.0' // default: localhost
   },
 
-  matomo: {
-    matomoUrl: 'https://analytics.firstvoices.com/',
-    siteId: 6,
-    enableLinkTracking: true
-  },
-
   env: {
     COGNITO_APP_CLIENT_ID: process.env.COGNITO_APP_CLIENT_ID,
     COGNITO_URL: process.env.COGNITO_URL,
