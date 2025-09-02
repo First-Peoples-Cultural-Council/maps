f0b2888407e51f1f75fcdfcce02b10eb3e9b217a
rohit-fpcc <rsharma@fpcc.ca>
2025-09-02 16:12:00 -0700

    Move monkey patch to the start of entry point

--- web/web/wsgi.py
+++ web/web/wsgi.py
@@ -4,11 +4,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
-
-import os
-
 from gevent import monkey
 monkey.patch_all()
+
+import os
 
 from django.core.wsgi import get_wsgi_application
 
