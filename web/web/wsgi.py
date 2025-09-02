cd369fbf1bb6e0328eb5d5ca21b8d620f0b78d61
rohit-fpcc <rsharma@fpcc.ca>
2025-09-02 11:56:54 -0700

    Revert "Replace standard blocking calls with non-blocking greenlet using gevent"
    This reverts commit 1e31e5659c2644717831b21392b25ddf9b1f3bdb.

--- web/web/wsgi.py
+++ web/web/wsgi.py
@@ -7,9 +7,6 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 
-from gevent import monkey
-monkey.patch_all()
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web.settings")
