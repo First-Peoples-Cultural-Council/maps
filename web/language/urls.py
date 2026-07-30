c90adbb3ba77dc8c959f379702e5e1b63193c476
Sahil Sharma <sahil@fpcc.ca>
2026-07-30 15:28:36 -0700

    Updated to django 4.2, pg12.3, and associated dependencies

--- web/language/urls.py
+++ web/language/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import re_path
 from rest_framework import routers
 
 from .views import (
@@ -48,20 +48,20 @@ router.register(r"notification", NotificationViewSet, basename="notification")
 router.register(r"recording", RecordingViewSet, basename="recording")
 
 urlpatterns = [
-    url("language-geo/$", LanguageGeoList.as_view(), name="language-geo"),
-    url("community-geo/$", CommunityGeoList.as_view(), name="community-geo"),
-    url("placename-geo/$", PlaceNameGeoList.as_view(), name="placename-geo"),
-    url("art-geo/$", ArtGeoList.as_view(), name="art-geo"),
-    url("language-search/$", LanguageSearchList.as_view(), name="language-search"),
-    url("community-search/$", CommunitySearchList.as_view(), name="community-search"),
-    url("placename-search/$", PlaceNameSearchList.as_view(), name="placename-search"),
-    url("art-search/$", ArtSearchList.as_view(), name="art-search"),
-    url("arts/public-art", PublicArtList.as_view(), name="arts-public-art"),
-    url("arts/artist", ArtistList.as_view(), name="arts-artist"),
-    url("arts/event", EventList.as_view(), name="arts-event"),
-    url("arts/organization", OrganizationList.as_view(), name="arts-organization"),
-    url("arts/resource", ResourceList.as_view(), name="arts-resource"),
-    url("arts/grant", GrantList.as_view(), name="arts-grant"),
-    url("arts/placename", ArtworkPlaceNameList.as_view(), name="artwork-placename"),
-    url("arts/artwork", ArtworkList.as_view(), name="arts-artwork")
+    re_path(r"language-geo/$", LanguageGeoList.as_view(), name="language-geo"),
+    re_path(r"community-geo/$", CommunityGeoList.as_view(), name="community-geo"),
+    re_path(r"placename-geo/$", PlaceNameGeoList.as_view(), name="placename-geo"),
+    re_path(r"art-geo/$", ArtGeoList.as_view(), name="art-geo"),
+    re_path(r"language-search/$", LanguageSearchList.as_view(), name="language-search"),
+    re_path(r"community-search/$", CommunitySearchList.as_view(), name="community-search"),
+    re_path(r"placename-search/$", PlaceNameSearchList.as_view(), name="placename-search"),
+    re_path(r"art-search/$", ArtSearchList.as_view(), name="art-search"),
+    re_path(r"arts/public-art", PublicArtList.as_view(), name="arts-public-art"),
+    re_path(r"arts/artist", ArtistList.as_view(), name="arts-artist"),
+    re_path(r"arts/event", EventList.as_view(), name="arts-event"),
+    re_path(r"arts/organization", OrganizationList.as_view(), name="arts-organization"),
+    re_path(r"arts/resource", ResourceList.as_view(), name="arts-resource"),
+    re_path(r"arts/grant", GrantList.as_view(), name="arts-grant"),
+    re_path(r"arts/placename", ArtworkPlaceNameList.as_view(), name="artwork-placename"),
+    re_path(r"arts/artwork", ArtworkList.as_view(), name="arts-artwork"),
 ] + router.urls
