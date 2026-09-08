70f4004599548e4a3096eba89b5724a7e79a58bd
rohit-fpcc <rsharma@fpcc.ca>
2026-09-08 12:28:32 -0700

    Parameterize reverse proxy conf

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/ash
 set -eu
 
-envsubst '${CACHE_LIFE}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${CACHE_LIFE} ${SERVER_NAMES} ${REDIRECT_HOST}' \
+  < /etc/nginx/conf.d/default.conf.template \
+  > /etc/nginx/conf.d/default.conf
 
 exec "$@"
 
